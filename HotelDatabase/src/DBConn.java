d61e7c805e973d23aa25f9a3fc12e92f84428693
Pooja Sardesai <pmsardes@gmail.com>
2013-03-24 15:41:42 -0400

    Changed main to method.

--- HotelDatabase/src/DBConn.java
+++ HotelDatabase/src/DBConn.java
@@ -2,16 +2,32 @@ import java.sql.*;
 
 public class DBConn {
 	
-	public static Connection main(String args[]) throws Exception
-	{
-		Class.forName("com.mysql.jdbc.Driver"); 
-		String url = "jdbc:mysql://localhost/test"; 
-		String username = "root"; 
-		String pwd = ""; 
-		
-		Connection conn = DriverManager.getConnection(url, username, pwd);
-		return conn; 
+	public void getConnection() {
+		System.out.println("Connecting to JDBC...");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Successfully connected.");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error: No Driver!");
+			e.printStackTrace();
+			return;
+		}
+	 
+		Connection connection = null;
+		try {
+			connection = DriverManager
+			.getConnection("jdbc:mysql://localhost:3306/", "root", "mysql");
+		} catch (SQLException e) {
+			System.out.println("Error: Cannot connect!");
+			e.printStackTrace();
+			return;
+		}
+	 
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Cannot find database.");
+		}
 		
 	}
-	
 }
