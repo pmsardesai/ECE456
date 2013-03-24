41987d5b063d0a0ec163ae67588866c48a7eb9a9
saladi <shweta.aladi@gmail.com>
2013-03-24 17:33:18 -0400

    IO for question 1

--- HotelDatabase/src/Assignment3.java
+++ HotelDatabase/src/Assignment3.java
@@ -5,10 +5,13 @@ import java.io.InputStreamReader;
 import com.mysql.jdbc.*;
 
 public class Assignment3 {
-
+	
+	//global variables
+	static Connection conn; 
+	static BufferedReader bufferRead;
 	public static void main(String args[]){
 		
-		Connection conn = null; 
+		//Connection conn = null; 
 		//get database connection 
 		try {
 			conn = (Connection) DBConn.getConnection();
@@ -16,50 +19,99 @@ public class Assignment3 {
 			// TODO Auto-generated catch block
 			//e1.printStackTrace();
 			System.out.println("No Database Connection...exiting...");
-			//return; 
+			return; 
 		} 
 		
-		
-		// console IO
-		System.out.println("Welcome to the Hotel Database");
-		System.out.println("Menu: \n(A) Add Guest \n(B) Delete Guest \n(C) Update Guest");
-		 
-		try{
-		    BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-		    String s = bufferRead.readLine();
-		    
-		    char a = s.toUpperCase().charAt(0); 
-		    System.out.println("Selected Option:" + a);
-
-		    switch(a){
-		    case 'A':
-		    	//Get guestname, address
-		    	System.out.println("Please enter the guest's name (Firstname Lastname)");
-		        String name = bufferRead.readLine();
-		        
-		        System.out.println("Please enter the guest's address");
-		        String address = bufferRead.readLine();
-				
-		    	//call add guest
-		    	Guest.add(conn, name, address);
-		    	break; 
-		    case 'B':
-		    	//call delete guest
-		    	break; 
-		    case 'C':
-		    	//call update guest
-		    	break;
-		    
-		    }
-		    
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
-		catch(Exception e){
-			e.printStackTrace(); 
+		while(true){
+			// console IO
+			System.out.println("Welcome to the Hotel Database");
+			System.out.println("Menu: \n(A) Add Guest \n(B) Delete Guest \n(C) Update Guest");
+			 
+			try{
+			    bufferRead = new BufferedReader(new InputStreamReader(System.in));
+			    String s = bufferRead.readLine();
+			    
+			    char a = s.toUpperCase().charAt(0); 
+			    System.out.println("Selected Option:" + a);
+	
+			    switch(a){
+			    case 'A': //add guest
+			    	addGuest(); 
+			    	break; 
+			    case 'B': //delete guest
+			    	deleteGuest(); 
+			    	break; 
+			    case 'C': //update guest
+			    	updateGuest(); 
+			    	break;
+			    default:
+			    	System.out.println("That option does not exist.");
+			    	break; 
+			    }
+			    
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+			catch(Exception e){
+				e.printStackTrace(); 
+			}
 		}
 	}
 	
+	
+	public static void addGuest() throws IOException, Exception{
+		//Get guestname, address
+	
+		System.out.println("Please enter the guest's name (Firstname Lastname)");
+        String name = bufferRead.readLine();	
+        
+        while(Guest.isNameValid(name)){
+        	System.out.println("You entered: \"" + name + "\", which " + 
+        			"is invalid. Please try again.");
+	        name = bufferRead.readLine();
+        }
+        
+        System.out.println("Please enter the guest's address");
+        String address = bufferRead.readLine();
+		
+    	//call add guest
+    	Guest.add(conn, name, address);
+
+		return; 
+	}
+
+	public static void deleteGuest() throws IOException, Exception{
+
+    	System.out.println("Please enter the guest id:");
+        String id = bufferRead.readLine();
+        
+        while(! Guest.isGuestIdValid(conn, id)){
+        	System.out.println("You entered: \"" + id + "\", which " + 
+        			"is invalid. Please try again.");
+	        id = bufferRead.readLine();
+        }
+        
+    	//call delete guest
+    	Guest.delete(conn, id);
+	}
+
+	public static void updateGuest() throws IOException, Exception{
+		//call update guest
+    	System.out.println("Please enter the guest's name (Firstname Lastname)");
+        String name = bufferRead.readLine();
+        
+        while(Guest.isNameValid(name)){
+        	System.out.println("You entered: \"" + name + "\", which " + 
+        			"is invalid. Please try again.");
+	        name = bufferRead.readLine();
+        }
+        
+        System.out.println("Please enter the guest's address");
+        String address = bufferRead.readLine();
+		
+    	//call add guest
+    	Guest.add(conn, name, address);
+	}
 }
