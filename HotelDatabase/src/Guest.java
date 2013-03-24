00d20f97a40415eac5a4f97b608650c826bfb23b
Pooja Sardesai <pmsardes@gmail.com>
2013-03-24 16:27:09 -0400

    Guest add complete.

--- HotelDatabase/src/Guest.java
+++ HotelDatabase/src/Guest.java
@@ -1,14 +1,31 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import com.mysql.*;
 import com.mysql.jdbc.Connection;
 
 
 public class Guest {
-	
-	public static void add(Connection conn, String username, String address)
-		throws Exception{
-		
-		
-		
+	public static void add(Connection conn, String name, String address) {
+		try {
+			PreparedStatement ps = conn.prepareStatement(
+					"SELECT guestID FROM guest ORDER BY guestID DESC LIMIT 1");
+			ResultSet rs = ps.executeQuery();
+
+			int lastGuestID = 1;
+			if(rs.next()) {
+			  lastGuestID = Integer.parseInt(rs.getString(1));
+			}
+			
+			Statement s = conn.createStatement();
+			s.executeUpdate("INSERT INTO guest (guestID, guestAddress, guestName) " +
+					  "VALUES ('lastGuestID', 'address', 'name')");	
+		} catch (SQLException e) {
+			System.out.println("Error: Could not add guest.");
+			e.printStackTrace();
+		}
+		System.out.println("Successfully added guest.");
 	}
 	
 	public static void update(){
@@ -17,7 +34,5 @@ public class Guest {
 	
 	public static void delete(){
 		
-	}
-	
-	
+	}	
 }
