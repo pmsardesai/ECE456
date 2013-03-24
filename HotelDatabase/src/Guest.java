795f75e2f30d7f10b9d038ffc1c6a12c461f8bc9
saladi <shweta.aladi@gmail.com>
2013-03-24 15:13:21 -0400

    Adding project files

---
+++ HotelDatabase/src/Guest.java
@@ -0,0 +1,16 @@
+import com.mysql.*;
+import com.mysql.jdbc.Connection;
+
+
+public class Guest {
+	
+	public static void add(Connection conn, String username, String address)
+		throws Exception{
+		
+		
+		
+	}
+	
+	
+	
+}
