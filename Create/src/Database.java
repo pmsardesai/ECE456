8d7f2fccd9c2e04a3174c80beec9ef3999f9d8c3
Pooja Sardesai <pmsardes@gmail.com>
2013-03-25 20:39:07 -0400

    Can create tables and databases.

--- Create/src/Database.java
+++ Create/src/Database.java
@@ -12,7 +12,20 @@ public class Database {
 	public static void main(String args[]) {
 		try {
 			conn = (Connection)getConnection();
-			createDatabase(conn);
+			if (conn != null) {
+				System.out.println("Connection successful.");
+			} else {
+				System.out.println("Connection not successful.");
+				return;
+			}
+			
+			String name = "HotelDatabase2";
+			
+			createDatabase(conn, name);
+			conn.close();
+			
+			// create new connection
+			conn = getConnectionWithNewDatabase(name);
 			populateTables(conn);
 		} catch (Exception e1) {
 			System.out.println("Error: No Database Connection. Exiting...");
@@ -30,13 +43,27 @@ public class Database {
 		Connection conn = (Connection) DriverManager.getConnection(url, username, pwd);
 		return conn; 
 	}	
-	public static void createDatabase(Connection conn) {
+	
+	public static Connection getConnectionWithNewDatabase(String name) throws Exception
+	{
+		Class.forName("com.mysql.jdbc.Driver"); 
+		String url = "jdbc:mysql://localhost:3306/" + name; 
+		String username = "root"; 
+		String pwd = "mysql"; 
+		
+		Connection conn = (Connection) DriverManager.getConnection(url, username, pwd);
+		return conn; 
+	}
+	
+	public static void createDatabase(Connection conn, String name) {
 		try {
+			System.out.println("Creating database...");
 			Statement stmt = conn.createStatement();
-			String sql = "CREATE DATABASE hoteldatabase2";
+			String sql = "CREATE DATABASE " + name;
 		    stmt.executeUpdate(sql);
+		    System.out.println("Done.");
 		} catch (Exception e) {
-			
+			System.out.println("Failed to make a database.");
 		}
 	}
 	
@@ -61,8 +88,6 @@ public class Database {
 			      "	  price DECIMAL(5,2) NULL CHECK(price BETWEEN 50.00 AND 250.00), " +
 				  "   type CHAR(6) NULL CHECK (type IN ('Single','Double','Queen','King')), " +
 				  "	  PRIMARY KEY (hotelID, roomNo), " +
-				  "	  INDEX `hotelID_idx` (hotelID ASC), " +
-				  "   CONSTRAINT `fk_hotelID_Room` " +
 				  "   FOREIGN KEY (hotelID) REFERENCES Hotel(hotelID) " +
 				  "      ON DELETE NO ACTION " +
 				  "		 ON UPDATE NO ACTION " +
@@ -81,28 +106,20 @@ public class Database {
 			
 			// create Booking table
 			sql = "CREATE TABLE Booking ( " +
+			      "   bookingID CHAR(4) NOT NULL, " +
 				  "   hotelID CHAR(4) NOT NULL, " +
 				  "   guestID CHAR(4) NOT NULL, " +
 				  "   roomNo CHAR(4) NOT NULL, " +
 				  "   startDate DATE NOT NULL, " +
 				  "   endDate DATE NULL, " +
-				  "	PRIMARY KEY (hotelID, guestID, roomNo, startDate), " +
-			      " INDEX `fk_hotelID_bookinglog_idx` (hotelID ASC), " +
-				  " INDEX `fk_guestID_bookinglog_idx` (guestID ASC), " +
-				  " INDEX `fk_roomNo_bookinglog_idx` (hotelID ASC, roomNo ASC), " +
-				  " CONSTRAINT `fk_hotelID_booking` " +
-				  "		FOREIGN KEY (hotelID)" +
-				  "		REFERENCES Hotel (hotelID) " +
+				  "	PRIMARY KEY (bookingID, hotelID, guestID, roomNo, startDate), " +
+				  "	FOREIGN KEY (hotelID) REFERENCES Hotel (hotelID) " +
 				  "		ON DELETE NO ACTION " +
 				  "		ON UPDATE NO ACTION, " +
-				  " CONSTRAINT `fk_guestID_booking` " +
-				  "		FOREIGN KEY (guestID) " +
-				  "     REFERENCES Guest (guestID) " +
+				  "	FOREIGN KEY (guestID) REFERENCES Guest (guestID) " +
 				  "     ON DELETE NO ACTION " +
 				  "		ON UPDATE NO ACTION, " +
-				  " CONSTRAINT `fk_roomNo_booking` " +
-				  " FOREIGN KEY (hotelID, roomNo) " +
-				  "     REFERENCES Room (hotelID, roomNo) " +
+				  " FOREIGN KEY (hotelID, roomNo) REFERENCES Room (hotelID, roomNo) " +
 				  "     ON DELETE NO ACTION " +
 				  "     ON UPDATE NO ACTION);";
 			stmt.executeUpdate(sql);
@@ -116,11 +133,21 @@ public class Database {
 				  "  totalDays INT NOT NULL, " +
 				  "  totalPrice DECIMAL NOT NULL, " +
 				  "  PRIMARY KEY (billingID), " +
-				  "  FOREIGN KEY (hotelID, roomNo, guestID) REFERENCES Booking(hotelID, roomNo, guestID)" +
-				  " );";
+				  "	 FOREIGN KEY (hotelID) REFERENCES Hotel (hotelID) " +
+				  "		ON DELETE NO ACTION " +
+				  "		ON UPDATE NO ACTION, " +
+				  "	 FOREIGN KEY (guestID) REFERENCES Guest (guestID) " +
+				  "     ON DELETE NO ACTION " +
+				  "		ON UPDATE NO ACTION, " +
+				  "  FOREIGN KEY (hotelID, roomNo) REFERENCES Room (hotelID, roomNo) " +
+				  "     ON DELETE NO ACTION " +
+				  "     ON UPDATE NO ACTION " +
+				  ");";
 			stmt.executeUpdate(sql);
-		} catch (SQLException e) {
 			
+			System.out.println("Done.");
+		} catch (SQLException e) {
+			System.out.println("Failed to make the tables.");
 		}		
 	}	
 }
